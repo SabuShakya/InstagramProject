c65e464cddb4044dac6695c96340cde900ed559e
SabuShakya <sabushakya1@gmail.com>
2018-01-11 12:00:27 +0545

    changes in user

--- Instagram/User/src/main/webapp/static/js/controller/SignupController.js
+++ Instagram/User/src/main/webapp/static/js/controller/SignupController.js
@@ -1,21 +1,21 @@
 (function(){
     angular.module('userModule').controller('SignupController',SignupController);
+
     SignupController.$inject =['SignupService','$location'];
     function SignupController(SignupService, $location){
-
         var vm=this;
-            vm.firstName ='';
-            vm.lastName ='';
-            vm.uname ='';
-            vm.email='';
-            vm.password='';
-            vm.repassword='';
-            
-            vm.match =false;
-            vm.error_msg = false;
-            if(!(vm.password == vm.repassword)){
-                vm.match = true;
-            }
+        vm.firstName ='';
+        vm.lastName ='';
+        vm.uname ='';
+        vm.email='';
+        vm.password='';
+        vm.repassword='';
+
+        vm.match =false;
+        vm.error_msg = false;
+        if(!(vm.password == vm.repassword)){
+            vm.match = true;
+        }
         vm.url="/signup";
         vm.createUser= createUser;
 
@@ -27,14 +27,12 @@
                 'email': vm.email,
                 'password': vm.password
             };
-
-            SignupService.createUser(vm.url,vm.newUser).then(function(value){
+            SignupService.signupUser(vm.url,vm.newUser).then(function(value){
                     console.log("success");
                     $location.path("/login");
                 }, function(reason){
                     vm.error_msg = true;
-                }
-            );
+                });
         }
     }
 })();
