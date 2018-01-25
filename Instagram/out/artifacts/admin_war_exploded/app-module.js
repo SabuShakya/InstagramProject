b9c0557a059bf54635544b86e7532f97f3209de1
smreeti <mool.smreeti@gmail.com>
2018-01-25 13:55:11 +0545

    following

--- Instagram/out/artifacts/admin_war_exploded/app-module.js
+++ Instagram/out/artifacts/admin_war_exploded/app-module.js
@@ -3,19 +3,19 @@
     angular.module('adminModule').config(['$routeProvider',function ($routeProvider) {
         $routeProvider.when("/login",{
             controller:'LoginController as login',
-            templateUrl:'/modules/views/loginPage.jsp'
+            templateUrl:'modules/views/loginPage.jsp'
             })
             .when("/signup",{
                 controller:'SignupController as signup',
-                templateUrl:'/modules/views/signupPage.jsp'
+                templateUrl:'modules/views/signupPage.jsp'
             })
             .when("/adminPage",{
                 controller:"AdminpageController as admin",
-                templateUrl:'/modules/views/adminPage.jsp'
+                templateUrl:'modules/views/adminPage.jsp'
             })
             .when("/addAdmin",{
                 controller:"NewAdminController as newadmin",
-                templateUrl:'/modules/views/addNewAdmin.jsp'
+                templateUrl:'modules/views/addNewAdmin.jsp'
             })
             // .when("/admin/adminDashboard",{
             //     controller:"AdminPageController as admin",
